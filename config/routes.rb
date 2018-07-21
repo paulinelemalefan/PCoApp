501c4a7b3fd5478983b4dbe20dc270fa9d37c055
paulinelemalefan <pauline.lemalefan@gmail.com>
2018-07-21 13:12:58 +0200

    initial commit with gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
  resources :startups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# je mets des commentaires
 end
